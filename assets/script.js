babf4874bae90906d05fbda02a1ce1282633d59d
Trey Tayler Smith <treytaylersmith@gmail.com>
2024-05-15 19:41:47 -0500

    added JavaScript funtionality

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,32 @@
 const topic = ['HTML', 'CSS', 'Git','Javascript'];
 
-for(x=0; x<topic.length; x++) {
-    console.log(topic[x]);
+const randomTopic = topic[Math.floor(Math.random()* topic.length)];
+
+
+
+function listTopics(){
+    for(x=0; x<topic.length; x++) {
+        console.log(topic[x]);
+    }
+}
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+
+
+function selectTopic(){
+    
+    if(randomTopic === 'HTML') console.log ("Let's study HTML");
+
+    else if (randomTopic === 'CSS') console.log ("Let's study CSS");
+
+    else if (randomTopic === 'Git') console.log ("Let's study Git");
+
+    else if (randomTopic === 'JavaScript') console.log ("Let's study JavaScript");
+
+    else console.log ('Please try again!');
+
 }
 
-/*
-if(topic === 'HTML') console.log ("Let's study HTML");
-
-else if (topic === 'CSS') console.log ("Let's study CSS");
-
-else if (topic === 'Git') console.log ("Let's study Git");
-
-else if (topic === 'JavaScript') console.log ("Let's study JavaScript");
-
-else console.log ('Please try again!');
- */
-
+console.log('Which topic should we study first?');
+selectTopic();
